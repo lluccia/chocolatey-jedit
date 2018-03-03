fa532435472dcda9a328f8835d4e8e023c680de2
Leandro Conca De Luccia <lluccia@gmail.com>
2018-03-03 13:57:10 -0300

    package version 5.0.0.002 downloaded from chocolatey

---
+++ jedit/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿$url='http://downloads.sourceforge.net/project/jedit/jedit/5.0.0/jedit5.0.0install.exe?r=http%3A%2F%2Fwww.jedit.org%2Findex.php%3Fpage%3Ddownload&ts=1370502027&use_mirror=heanet'
+
+Install-ChocolateyPackage "jEdit" exe '/verysilent' "$url"
