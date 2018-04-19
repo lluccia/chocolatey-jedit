b00a89c4d31fcbf6e1644652dc58ab43732bece3
Leandro Conca De Luccia <leandro.luccia@egs.com.br>
2018-04-19 15:35:58 -0300

    updates to version 5.5.0

--- jedit/tools/chocolateyInstall.ps1
+++ jedit/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://downloads.sourceforge.net/project/jedit/jedit/5.4.0/jedit5.4.0install.exe'
-  checksum      = '979f6eb54c388666858dc462ae344667104341f30d00218842ad603adc9dccb8'
+  url           = 'https://downloads.sourceforge.net/project/jedit/jedit/5.5.0/jedit5.5.0install.exe'
+  checksum      = 'ac6255eb30b3f26b3d4646f9f65a8641eb9d19240435eaf4b2fa6766a2d34d90'
   checksumType  = 'sha256'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
